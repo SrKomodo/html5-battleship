befb59aa3f0036dc04c768e6a0c6389c28858d22
SrKomodo <komodosr@gmail.com>
2017-10-04 23:13:46 -0300

    Add basic server for testing

---
+++ index.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+let http = require("http");
+
+let server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World!");
+});
+
+let port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
