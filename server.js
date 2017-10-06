30974f720a1f808b58bc32291bca17bfd61d3d2f
SrKomodo <komodosr@gmail.com>
2017-10-06 12:58:14 -0300

    just work

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ app.use(express.static("public"));
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-setTimeout(() => {
-  console.log(require("fs").readdir(__dirname));
-  console.log(require("fs").readdir(require("path").join(__dirname, "public")));
-}, 10000);
+const fs = require("fs");
+const path = require("path");
+setInterval(() => {
+  console.log(fs.readdir(__dirname));
+  console.log(fs.readdir(path.join(__dirname, "public")));
+}, 5000);
 
 http.listen(80, () => {
   console.log("app started on port 80");
