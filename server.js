6ecf01d0c72a7518f7a8c2f75068ef399e885341
SrKomodo <komodosr@gmail.com>
2017-10-06 12:53:31 -0300

    work

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ app.use(express.static("public"));
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-http.listen(80, () => {
+setTimeout(() => {
   console.log(require("fs").readdir(__dirname));
   console.log(require("fs").readdir(require("path").join(__dirname, "public")));
+}, 10000);
+
+http.listen(80, () => {
   console.log("app started on port 80");
 });
 
