5ddd1356d450fd30248632c3611828eb6064eecb
SrKomodo <komodosr@gmail.com>
2017-10-06 13:38:40 -0300

    pray

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 /* eslint-env node */
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// app.use(express.static("public"));
+const app = express();
+app.use(express.static("public"));
 
-// const http = require("http").Server(app);
-// const io = require("socket.io")(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const fs = require("fs");
-setInterval(() => {
-  fs.readdir(__dirname, (err, files) => {
-    if (err) throw err;
+fs.readdir(__dirname, (err, files) => {
+  if (err) throw err;
+  setInterval(() => {
     console.log("test");
     require("util").inspect(files);
-  });
-}, 5000);
+  }, 5000);
+});
 
-// http.listen(80, () => {
-//   console.log("app started on port 80");
-// });
+http.listen(80, () => {
+  console.log("app started on port 80");
+});
 
-// io.on("connection", () => {
-//   console.log("a user connected");
-// });
+io.on("connection", () => {
+  console.log("a user connected");
+});
