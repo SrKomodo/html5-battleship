c811b78d0bf88b8d4d561a31a6ec43d325546e56
SrKomodo <komodosr@gmail.com>
2017-10-06 13:16:27 -0300

    Testing to find out what the heck is going on

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 /* eslint-env node */
-const express = require("express");
+// const express = require("express");
 
-const app = express();
-app.use(express.static("public"));
+// const app = express();
+// app.use(express.static("public"));
 
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+// const http = require("http").Server(app);
+// const io = require("socket.io")(http);
 
 const fs = require("fs");
-const path = require("path");
 setInterval(() => {
   console.log(fs.readdir(__dirname));
-  console.log(fs.readdir(path.join(__dirname, "public")));
 }, 5000);
 
-http.listen(80, () => {
-  console.log("app started on port 80");
-});
+// http.listen(80, () => {
+//   console.log("app started on port 80");
+// });
 
-io.on("connection", () => {
-  console.log("a user connected");
-});
+// io.on("connection", () => {
+//   console.log("a user connected");
+// });
